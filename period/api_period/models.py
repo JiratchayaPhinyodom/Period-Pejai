3ec2697239c5a147058595bae58d0abdfc3c9cab
dzp-datah <damita.f@ku.th>
2022-10-21 17:48:19 +0700

    Create HomePage model

--- period/api_period/models.py
+++ period/api_period/models.py
@@ -1,6 +1,20 @@
+# Create your models here.
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
 
 
-# Create your models here.
+class HomePage(models.Model):
+    """Home Page model"""
+    diary_text = models.CharField(max_length=400)
+    blood_level = models.DateTimeField('store blood level')
+    pain_level = models.DateTimeField('store pain level')
+    current_period = True
 
+    def can_choose(self) -> bool:
+        """For checking the date that user has period"""
+        if not self.current_period:
+            return False
+
+    def __str__(self):
+        return self.diary_text
