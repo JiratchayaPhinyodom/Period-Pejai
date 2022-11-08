b565da0cb57dc14cc12e061455deae1457cb1151
Crparichaya <parichaya.yan@ku.th>
2022-11-08 15:20:03 +0700

    Fix some code

--- period/api_period/serializers.py
+++ period/api_period/serializers.py
@@ -1,4 +1,6 @@
 from rest_framework.serializers import Serializer, FileField
+from rest_framework import serializers
+from .models import Setting
 
 
 class UploadFile(Serializer):
@@ -6,12 +8,11 @@ class UploadFile(Serializer):
 
     class Meta:
         fields = ['file_uploaded']
-from rest_framework import serializers
-from .models import Setting
 
 
 class MyData(serializers.ModelSerializer):
     """To see the data in database file"""
+
     class Meta:
         model = Setting
         fields = ["birth_year", "period_length", "cycle_length", "luteal_length"]
