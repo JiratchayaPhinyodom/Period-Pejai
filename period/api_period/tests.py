4bb3aee8fcca01d79863f331243e02409e420e66
Crparichaya <parichaya.yan@ku.th>
2022-11-08 14:06:09 +0700

    Adding some test

--- period/api_period/tests.py
+++ period/api_period/tests.py
@@ -2,9 +2,18 @@ from django.test import TestCase
 from .models import Setting
 
 
-# Create your tests here.
-class SettingModelTests(TestCase):
+# class ModelSettingTestCase(TestCase):
+#     def SetUp(self):
+#         self.birth_year = Setting.birth_year()
+#
+#     def test_user_input_is_integer(self):
+#         Birth_year = 2002
+#         self.birth_year = Setting.birth_year()
+#         self.assertEqual(self.birth_year, Birth_year)
 
-    def test_uses_setting_template(self):
-        response = self.client.get('/')
-        self.assertTemplateUsed(response, 'setting/setting_page.html')
+
+class ViewsTestCase(TestCase):
+    def test_index_loads_properly(self):
+        """The index page loads properly"""
+        response = self.client.get('server_ip:8000')
+        self.assertEqual(response.status_code, 404)
