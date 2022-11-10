3da9990fcb4edbe33be409dabe0dbd52630416d1
Crparichaya <parichaya.yan@ku.th>
2022-11-10 15:01:20 +0700

    Update the test

--- period/api_period/tests.py
+++ period/api_period/tests.py
@@ -1,45 +1,45 @@
+import json
 from django.test import TestCase
+from rest_framework.reverse import reverse
 from .models import Setting
 from django.test import Client
+from rest_framework.test import APITestCase
 
 
 class ModelSettingTestCase(TestCase):
-    def setUp(self):
-        self.birth_year = 2002
-        self.period_length = 7
-        self.cycle_length = 28
-        self.luteal_length = 14
-
-    def test_user_input_is_integer(self):
-        self.assertEqual(self.birth_year, 2002)
-        self.assertEqual(self.period_length, 7)
-        self.assertEqual(self.cycle_length, 28)
-        self.assertEqual(self.luteal_length, 14)
-        # with self.assertRaises(TypeError):
-        #     self.assertEqual(self.luteal_length, "14") #failures
+    pass
 
 
 class ViewsTestCase(TestCase):
-    """Test that the pages has status code = 200 which mean it is ok"""
+    """Test that each page has status code = 200 which mean it is ok"""
     def setUp(self):
         # Every test needs a client.
         self.client = Client()
 
     def test_home_page(self):
         response = self.client.get('/api/home')
-
-        # Check that the response is 200 OK.
         self.assertEqual(response.status_code, 200)
 
     def test_setting_page(self):
         response = self.client.get('/api/setting')
-
-        # Check that the response is 200 OK.
         self.assertEqual(response.status_code, 200)
 
     def test_login_page(self):
         response = self.client.get('/api/login')
-
-        # Check that the response is 200 OK.
         self.assertEqual(response.status_code, 200)
 
+
+# class SerializesTestCase(TestCase):
+#     def test_contains_setting(self):
+
+
+# class TestDataAPIView(APITestCase):
+#     def test_create_account(self):
+#         """
+#         Ensure we can create a new account object.
+#         """
+#         url = reverse('account-list')
+#         data = {'name': 'DabApps'}
+#         json.dumps(data)
+#         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+#         self.assertEqual(response.data, data)
