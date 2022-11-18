e782a831e84fb341644afcf833f78c350a776781
jiratchayaphinyodom <jiratchaya.phiny@ku.th>
2022-11-18 10:53:49 +0700

    Remove some stuff that is no longer used in the code. #42

--- front-end/src/components/Login.js
+++ front-end/src/components/Login.js
@@ -20,18 +20,13 @@ export default function Login() {
     <div className="login">
       <div className="login-topper-form"></div>
         <div className="login-form">
-        <a href="http://localhost:3000/login">
-            <img src={LogoPic} className="App-logo" height = "350px"/>
-        </a>
-        {/* <span className="login-title"> */}
+          <img src={LogoPic} className="App-logo" height = "350px"/>
             <h1 className="head-login">Welcome to period-pejai</h1>
               <p1 className="paragraph-login">
               We'll assist you in overcoming
               the difficulty of forgetting your menstrual cycle.
               And foretell your period for the next month,
               As well as assist ladies who wish to plan for the birth of children.</p1>
-            {/* </span> */}
-          {/* {error && <Alert variant="danger">{error}</Alert>} */}
       <button onClick={async ()=>{await signInWithGoogle(); window.location.href = "/"}} className="login-google">Login in with google</button>
     </div>
   </div>
