fcaacc7969b5acb2ec7a5821f6febdbae7e08381
Pinpimarn Pinpisit <pinpimarn.p@ku.th>
2022-11-25 19:12:02 +0700

    Add

--- front-end/src/components/Login.js
+++ front-end/src/components/Login.js
@@ -15,20 +15,46 @@ export default function Login() {
   const [error, setError] = useState("")
   const [loading, setLoading] = useState(false)
   const history = useHistory()
+  const whatis = useRef(null);
+  const aboutus = useRef(null);
+  const login = useRef(null);
+
+  const scrollToSection = (elementRef) => {
+    window.scrollTo({
+      top: elementRef.current.offsetTop,
+      behavior: "smooth"
+    })
+  }
 
   return (
     <div className="login">
-      <div className="login-topper-form"></div>
-        <div className="login-form">
-          <img src={LogoPic} className="App-logo" height = "350px"/>
-            <h1 className="head-login">Welcome to period-pejai</h1>
-              <p1 className="paragraph-login">
-              We'll assist you in overcoming
-              the difficulty of forgetting your menstrual cycle.
-              And foretell your period for the next month,
-              As well as assist ladies who wish to plan for the birth of children.</p1>
-      <button onClick={async ()=>{await signInWithGoogle(); window.location.href = "/"}} className="login-google">Login in with Google Account</button>
-    </div>
+      <div className="hero">
+          <ul>
+            <li onClick={() => scrollToSection(whatis)} className="link"> What is Period Pejai?</li>
+            <li onClick={() => scrollToSection(aboutus)} className="link">About us</li>
+            <li onClick={() => scrollToSection(login)} className="link">Login</li>
+          </ul>
+          <div ref={whatis} className="what-is">
+            <h3>Period Pejai</h3>
+          </div>
+          <div ref={aboutus} className="about-us">
+            <h3>About us</h3>
+          </div>
+          <div ref={login} className="login-zone">
+            <h3>Login zone</h3>
+              <div className="login-topper-form"></div>
+                <div className="login-form">
+                  <img src={LogoPic} className="App-logo" height = "350px"/>
+                  <h1 className="head-login">Welcome to period-pejai</h1>
+                    <p1 className="paragraph-login">
+                    We'll assist you in overcoming
+                    the difficulty of forgetting your menstrual cycle.
+                    And foretell your period for the next month,
+                    As well as assist ladies who wish to plan for the birth of children.</p1>
+                    <button onClick={async ()=>{await signInWithGoogle(); window.location.href = "/"}} className="login-google">Login in with Google Account</button>
+                </div>
+              </div>
+          </div>
   </div>
   )
 }
