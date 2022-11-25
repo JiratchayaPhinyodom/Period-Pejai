8ccb560ca6663b7455f1f9648dcddf3ed417e1d5
dzp-datah <damita.f@ku.th>
2022-11-25 21:18:28 +0700

    update code #73

--- period/api_period/cron.py
+++ period/api_period/cron.py
@@ -1,6 +1,22 @@
-from .line_notify import *
-from .serializers import *
+import requests
+from datetime import datetime
 
-# Notification.objects.filter(uid=)
 
-send_notification('Your period will likely start in the next 3 days.', 'uid')
+def send_notification(message, token):  # เอาไว้ส่งข้อความพร้อมกับ token
+    url = 'https://notify-api.line.me/api/notify'
+    token = token
+    headers = {'content-type': 'application/x-www-form-urlencoded', 'Authorization': f'Bearer {token}'}
+
+    r = requests.post(url, headers=headers, data={'message': message})
+    return r.json()['status']
+
+
+uid_token = requests.get("localhost:8000/api/notification")
+for i in uid_token:
+    uid = i["uid"]
+    token = i["token"]
+    response = requests.get(f"localhost:8000/api/predict/?uid={uid}")
+    current_date = datetime.now()
+    next_three_day = datetime(response[0])
+    if current_date + 3 == next_three_day:
+        send_notification("message", token)
