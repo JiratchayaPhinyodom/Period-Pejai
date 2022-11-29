739088d672f2ae4bf5deecdfa78a9aae996e579b
Danita Frikaow <92835393+dzptahh@users.noreply.github.com>
2022-11-29 14:51:46 +0700

    Update redirect url #88

--- period/api_period/line_notify.py
+++ period/api_period/line_notify.py
@@ -1,25 +1,14 @@
-# import requests
-
-# url = 'https://notify-api.line.me/api/notify'
-# token = "IYsi0yC9Et4EqFHBzv9evCyN1azoebOgKkyU4UygHwj"
-# headers = {'content-type': 'application/x-www-form-urlencoded',
-# 'Authorization': 'Bearer ' + token}
-
-# msg = 'Hello LINE Notify'
-# r = requests.post(url, headers=headers, data={'message': msg})
-# print(r.text)
-
 import requests
 
 
-def get_access_token(code, uid):  # ได้ token ออกมา
+def get_access_token(code, uid):
     api_url = "https://notify-bot.line.me/oauth/token"
     content_type = "application/x-www-form-urlencoded"
 
     grant_type = "authorization_code"
     code = code
     uid = uid
-    redirect_uri = 'http://localhost:3000/'
+    redirect_uri = 'https://period-pejai-deploy.vercel.app/'
     client_id = '3i37SxxITCH1t4ngUNAPuz'
     client_secret = 'OL8v9BglCOlCvn1AVEpw2xwbsUuRU4iYRjES6jrL7GQ'
     headers = {'Content-Type': content_type}
